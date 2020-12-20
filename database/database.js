93acbb6824f4daeeb65e268f16c3df357d328e3d
Radulfr <41877997+radulfr-dev@users.noreply.github.com>
2020-12-20 20:03:44 +0100

    Included .env file

--- database/database.js
+++ database/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 let connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'raven'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 connection.connect();
