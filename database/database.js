2ea8e8d1cd0003a021e98ea4cdf1d2f239348129
Radulfr <41877997+radulfr-dev@users.noreply.github.com>
2020-12-22 13:24:38 +0100

    User register details are successfully being sent to server with hashed password

--- database/database.js
+++ database/database.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql');
+
 let connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
@@ -6,9 +7,44 @@ let connection = mysql.createConnection({
     database: process.env.DB_NAME
 });
 
-connection.connect();
 
-connection.query('SELECT * FROM test', function(error, results, fields){
-    if(error) throw error;
-    console.log(results);
-})
+function DatabaseController(){
+
+    function testConnection(){
+        //
+        // Test connection
+        connection.connect();
+
+        connection.query('SELECT * FROM test', function(error, results, fields){
+            if(error) throw error;
+            console.log(results);
+        });
+
+        connection.end();
+
+    }
+
+    function addUserToDatabase(userObject){
+
+        connection.connect();
+
+        connection.query(`INSERT INTO users (username, password, role, email, authorizaton) VALUES (${userObject.username}, ${userObject.password}, ${userObject.role}, ${userObject.email}, 1)`, function(error, results, fields){
+            if(error){
+                return {
+                    'status': 'error',
+                    'errorMessage': error
+                };
+            }
+
+            return {
+                'status': 'success',
+                'message': results.changedRows
+            };
+        });
+
+        connection.end();
+    }
+
+    return { addUserToDatabase };
+
+}
--- main.js
+++ main.js
@@ -2,10 +2,13 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const ravenDb = require('./database/database.js');
-//const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: false}));
+
+app.use(express.json());
 
 app.get('/', function(req, res){
     res.render('index');
@@ -20,16 +23,45 @@ app.get('/register', function(req, res){
 });
 
 app.post('/login', function(req, res){
+    const username = req.body.username;
+    const user = { name: username };
 
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET);
+    res.json({ accessToken: accessToken });
 });
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    })
+}
 
 app.post('/register', async function(req, res){
     try {
         //const hashedPassword = async bcrypt.hash(req.body.password, 10);
         // TODO: Add user to database here
+        //
+        console.log(req.body);
 
+        const salt = await bcrypt.genSalt();
+        const hashedPassword = await bcrypt.hash(req.body.password, salt);
+        console.log(salt);
+        console.log(hashedPassword);
+        const user = {
+            username: req.body.username,
+            password: hashedPassword,
+            role: req.body.role,
+            email: req.body.email
+        }
         res.redirect('/login');
-    }catch{
+    }catch(err){
+        console.log(err);
         res.redirect('/register');
     }
 });
