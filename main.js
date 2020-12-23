ea96a21216b459993a6134e67088fc79ecb0fab1
Radulfr <41877997+radulfr-dev@users.noreply.github.com>
2020-12-23 11:53:34 +0100

    Moved all routing functionality into middleware

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: false}));
-
 app.use(express.json());
 app.use(session({
     secret: process.env.ACCESS_TOKEN_SECRET,
@@ -20,88 +19,44 @@ app.use(session({
     saveUninitialized: false
 }));
 
+// Routes
+// ===============
+// GET
+
 app.get('/', authentication.checkUserIsAuthorized, function(req, res){
     res.render('index');
 });
 
-app.get('/login', function(req, res){
-    if(req.session.user){
-        res.redirect('/');
-    }else{
-        res.render('login');
-    }
+app.get('/login', authentication.redirectIfLoggedIn, function(req, res){
+    res.render('login');
 });
 
-app.get('/logout', function(req, res){
-    if(req.session.user){
-        delete req.session.user;
-    }
+app.get('/logout', authentication.logoutUser, function(req, res){
     res.redirect('/login');
-    
 });
 
-app.get('/register', function(req, res){
-    if(req.session.user && req.session.user['role'] === 'admin'){
-        res.render('register');
-    }
-    if(req.session.user && req.session.user['role'] === 'normal'){
-        res.redirect('/');
-    }
-    if(!req.session.user){
-        res.redirect('/login');
-    }
+app.get('/register', authentication.checkUserIsAdmin, function(req, res){
+    res.render('register');
 });
 
-app.post('/login', async function(req, res){
-    const username = req.body.username;
-    const usernameMatches = await ravenDb.checkDatabaseForUsername(username);
-
-    if(usernameMatches.length === 0){
-        return res.status(400).send('Cannot find user');
-    }
-    
-    const user = {
-        username: usernameMatches[0].username,
-        password: usernameMatches[0].password,
-        role: usernameMatches[0].role,
-        email: usernameMatches[0].email,
-        timeRegistered: usernameMatches[0]['time_registered'],
-        lastLogin: usernameMatches[0]['last_login'],
-        authorized: usernameMatches[0].authorized
-    };
-
-    try{
-        if(await bcrypt.compare(req.body.password, user.password)){
-            req.session.user = user;
-            res.redirect('/');
-        }else{
-            res.send('Not Allowed');
-        }
-    }catch(err){
-        console.log(err);
-        res.status(500).send();
-    }
-
-
+app.get('/top-up', authentication.checkUserIsAuthorized, function(req, res){
+    res.render('top-up');
 });
 
-app.post('/register', async function(req, res){
-    try {
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt);
-        const user = {
-            username: req.body.username,
-            password: hashedPassword,
-            role: req.body.role,
-            email: req.body.email
-        }
-        ravenDb.addUserToDatabase(user);
-        res.redirect('/login');
-    }catch(err){
-        console.log(err);
-        res.redirect('/register');
-    }
+// Routes
+// ===============
+// POST
+
+app.post('/login', authentication.loginUser, function(req, res){
+    res.redirect('/');
 });
+
+app.post('/register', authentication.registerUser, function(req, res){
+    res.redirect('/login');
+});
+
+// Start server
+// ===========================
 
 app.listen(3000, function(){
     console.log('Server is live on 3000');
--- middleware/authentication.js
+++ middleware/authentication.js
@@ -1,4 +1,8 @@
+const ravenDb = require('../database/database.js');
+const bcrypt = require('bcrypt');
+
 function Authentication(){
+
     function checkUserIsAuthorized(req, res, next){
 
         if(!req.session.user && !process.env.DEV) return res.redirect('/login');
@@ -6,7 +10,83 @@ function Authentication(){
         next();
 
     }
-    return { checkUserIsAuthorized };
+
+    function checkUserIsAdmin(req, res, next){
+
+        if(!req.session.user) return res.redirect('/login');
+
+        if(req.session.user['role'] === 'normal') return res.redirect('/');
+
+        next();
+    }
+
+    function logoutUser(req, res, next){
+        if(req.session.user) delete req.session.user;
+        next();
+    }
+
+    function redirectIfLoggedIn(req, res, next){
+        if(req.session.user) return res.redirect('/');
+        next();
+    }
+
+    async function loginUser(req, res, next){
+
+        const username = req.body.username;
+        const usernameMatches = await ravenDb.checkDatabaseForUsername(username);
+
+        if(usernameMatches.length === 0){
+            return res.status(400).send('Cannot find user');
+        }
+
+        const user = {
+            username: usernameMatches[0].username,
+            password: usernameMatches[0].password,
+            role: usernameMatches[0].role,
+            email: usernameMatches[0].email,
+            timeRegistered: usernameMatches[0]['time_registered'],
+            lastLogin: usernameMatches[0]['last_login'],
+            authorized: usernameMatches[0].authorized
+        };
+
+        try{
+            if(await bcrypt.compare(req.body.password, user.password)){
+                req.session.user = user;
+                next();
+            }else{
+                res.send('Not Allowed');
+            }
+        }catch(err){
+            console.log(err);
+            res.status(500).send();
+        }
+
+
+    }
+
+    async function registerUser(req, res, next){
+
+        try {
+            const salt = await bcrypt.genSalt();
+            const hashedPassword = await bcrypt.hash(req.body.password, salt);
+            const user = {
+                username: req.body.username,
+                password: hashedPassword,
+                role: req.body.role,
+                email: req.body.email
+            }
+            let databaseResponse = await ravenDb.addUserToDatabase(user);
+            console.log('databaseResponse');
+            console.log(databaseResponse);
+            next();
+        }catch(err){
+            console.log(err);
+            res.redirect('/register');
+        }
+    }
+
+    return { checkUserIsAuthorized, checkUserIsAdmin, logoutUser, redirectIfLoggedIn, loginUser, registerUser };
+
 }
 
 module.exports = Authentication();
